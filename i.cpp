81026bb95d45f194ee875622692d81aef6299519
LCHIN7X <lawchinxuan@gmail.com>
2025-05-30 14:11:28 +0800

    steel gpt

---
+++ i.cpp
@@ -0,0 +1,13 @@
+//n choose k
+//n!/(k!(n-k)!) 
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+const int mod = le9 +7;
+
+long long modInverse(long long a,long long mod){
+    long long res = 1;
+    long long power = mod -2;
+}
